c071945bee6261fb2f643aa7d31e94b917c53b50
Diego-Carrillo <carrillo.diego@uabc.edu.mx>
2023-12-09 01:35:43 -0800

    librerias

--- AUTOS.py
+++ AUTOS.py
@@ -13,87 +13,37 @@ db = 'concesionaria'
 # Creando la conexión
 engine = create_engine(f'mysql+pymysql://{usuario}:{contraseña}@{host}:{puerto}/{db}')
 
-
-sitio = 'https://www.cars.com/shopping/results/?stock_type=cpo&makes%5B%'
-respuesta = requests.get(sitio)
-respuesta.status_code
-soup = BeautifulSoup(respuesta.content, 'html.parser')
-results = soup.find_all('div', {'class' : 'vehicle-card'})
-len(results)
-
-#Nombre
-#Millaje
-#Nombre marca
-#Calificacion
-#Contador calificacion
-#Precio
-
+# Lista para almacenar los datos
 name = []
 mileage = []
 dealer_name = []
 rating = []
 review_count = []
 price = []
+
 for i in range(1, 11):
-
-    # website in variable
-    website = 'https://www.cars.com/shopping/results/?page=' + str(
-        i) + '&page_size=20&dealer_id=&list_price_max=&list_price_min=&makes[]=mercedes_benz&maximum_distance=20&mileage_max=&sort=best_match_desc&stock_type=cpo&year_max=&year_min=&zip='
-
-    # request to website
+    website = 'https://www.cars.com/shopping/results/?page=' + str(i) + '&page_size=20&dealer_id=&list_price_max=&list_price_min=&makes[]=mercedes_benz&maximum_distance=20&mileage_max=&sort=best_match_desc&stock_type=cpo&year_max=&year_min=&zip='
     response = requests.get(website)
-
-    # soup object
     soup = BeautifulSoup(response.content, 'html.parser')
-
-    # results
     results = soup.find_all('div', {'class': 'vehicle-card'})
 
-    # loop through results
     for result in results:
+        # Extracción y almacenamiento de datos
+        name.append(result.find('h2').get_text() if result.find('h2') else 'n/a')
+        mileage.append(result.find('div', {'class': 'mileage'}).get_text() if result.find('div', {'class': 'mileage'}) else 'n/a')
+        dealer_name.append(result.find('div', {'class': 'dealer-name'}).get_text().strip() if result.find('div', {'class': 'dealer-name'}) else 'n/a')
+        rating.append(result.find('span', {'class': 'sds-rating__count'}).get_text() if result.find('span', {'class': 'sds-rating__count'}) else 'n/a')
+        review_count.append(result.find('span', {'class': 'sds-rating__link'}).get_text() if result.find('span', {'class': 'sds-rating__link'}) else 'n/a')
+        price.append(result.find('span', {'class': 'primary-price'}).get_text() if result.find('span', {'class': 'primary-price'}) else 'n/a')
 
-        # name
-        try:
-            name.append(result.find('h2').get_text())
-        except:
-            name.append('n/a')
+# Creación del DataFrame
+Concesionaria = pd.DataFrame({'Nombre': name, 'Millaje': mileage, "Concesionaria": dealer_name, "Calificacion": rating, "Numero de Reviews": review_count, "Precio": price})
 
-        # mileage
-        try:
-            mileage.append(result.find('div', {'class': 'mileage'}).get_text())
-        except:
-            mileage.append('n/a')
+# Limpieza y conversión de tipos de datos
+Concesionaria['Millaje'] = Concesionaria['Millaje'].str.replace(r'\D', '', regex=True).astype(float)
+Concesionaria['Calificacion'] = Concesionaria['Calificacion'].astype(float)
+Concesionaria['Numero de Reviews'] = Concesionaria['Numero de Reviews'].str.replace(r'\D', '', regex=True).astype(int)
+Concesionaria['Precio'] = Concesionaria['Precio'].str.replace(r'\D', '', regex=True).astype(float)
 
-        # dealer_name
-        try:
-            dealer_name.append(result.find('div', {'class': 'dealer-name'}).get_text().strip())
-        except:
-            dealer_name.append('n/a')
-
-        # rating
-        try:
-            rating.append(result.find('span', {'class': 'sds-rating__count'}).get_text())
-        except:
-            rating.append('n/a')
-
-        # review_count
-        try:
-            review_count.append(result.find('span', {'class': 'sds-rating__link'}).get_text())
-        except:
-            review_count.append('n/a')
-
-        # price
-        try:
-            price.append(result.find('span', {'class': 'primary-price'}).get_text())
-        except:
-            price.append('n/a')
-#Diccionario
-Concesionaria= pd.DataFrame({'Nombre': name, 'Millaje':mileage, "Marca": dealer_name,"Calificacion":rating,
-                            "Numero de Reviews":review_count, "Precio":price})
-
-print(Concesionaria)
-
-Concesionaria['Numero de Reviews'] = Concesionaria['Numero de Reviews'].apply(lambda x: x.strip('reviews)').strip('('))
-
-
+# Insertar datos en MySQL
 Concesionaria.to_sql('concesionaria', con=engine, index=False, if_exists='append')
--- DASHBOARD_AUTOS.py
+++ DASHBOARD_AUTOS.py
@@ -1,10 +1,82 @@
-from sqlalchemy import create_engine #Un ejemplo venia con esta libreria y se me hace mas simple que el conector
-import pandas as pd
-import plotly.express as px
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
-from dash import Dash, dcc, html, dash_table, Input, Output, callback
+import plotly.express as px
+import pandas as pd
+from sqlalchemy import create_engine
 
+# Función para crear el dashboard
+def create_dashboard():
+    app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-engine = create_engine('mysql+pymysql://root:Airsoud99.@host:3306/concesionaria')
-df = pd.read_sql_query("SELECT * FROM concesionaria", engine)
+    # Conexión a la base de datos y carga de datos
+    engine = create_engine('mysql+pymysql://root:Airsoud99.@localhost:3306/concesionaria')
+    df = pd.read_sql_query("SELECT * FROM concesionaria", engine)
 
+    # Calcular estadísticas descriptivas
+    media_precio = df['Precio'].mean()
+    mediana_precio = df['Precio'].median()
+    moda_precio = df['Precio'].mode()[0]
+    desviacion_estandar_precio = df['Precio'].std()
+    rango_precio = df['Precio'].max() - df['Precio'].min()
+
+    # Dropdown para seleccionar la marca
+    marcas = df['Marca'].unique()
+    dropdown_marcas = dcc.Dropdown(
+        id='marca-dropdown',
+        options=[{'label': marca, 'value': marca} for marca in marcas],
+        value=marcas[0],
+        clearable=False,
+        style={'color': 'black'}
+    )
+
+    # Estilo para el fondo negro y texto claro
+    style_black_background = {
+        'backgroundColor': 'black',
+        'color': 'white',
+        'padding': '10px'
+    }
+
+    # Layout de la aplicación Dash
+    app.layout = dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H1("Dashboard de Concesionaria", style=style_black_background), width=12)
+        ]),
+        dbc.Row([
+            dbc.Col([
+                html.Label("Selecciona una Marca:", style=style_black_background),
+                dropdown_marcas
+            ], width=4),
+            dbc.Col([
+                html.Div([
+                    html.H3("Estadísticas de Precios", style=style_black_background),
+                    html.P(f"Media: {media_precio:.2f}", style=style_black_background),
+                    html.P(f"Mediana: {mediana_precio:.2f}", style=style_black_background),
+                    html.P(f"Moda: {moda_precio:.2f}", style=style_black_background),
+                    html.P(f"Desviación Estándar: {desviacion_estandar_precio:.2f}", style=style_black_background),
+                    html.P(f"Rango: {rango_precio:.2f}", style=style_black_background)
+                ])
+            ], width=8)
+        ], style=style_black_background),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='graph-precio'), width=6),
+            dbc.Col(dcc.Graph(id='graph-calificacion'), width=6)
+        ], style=style_black_background)
+    ], fluid=True, style=style_black_background)
+
+    # Callback para actualizar el gráfico de precio basado en la selección de la marca
+    @app.callback(
+        Output('graph-precio', 'figure'),
+        Input('marca-dropdown', 'value')
+    )
+    def update_graph(selected_marca):
+        filtered_df = df[df['Marca'] == selected_marca]
+        fig = px.bar(filtered_df, x='Nombre', y='Precio', title=f'Precios de los Vehículos de {selected_marca}')
+        fig.update_layout(plot_bgcolor='black', paper_bgcolor='black', font_color='white')
+        return fig
+
+    return app
+
+# Ejecutar la aplicación
+if __name__ == '__main__':
+    app = create_dashboard()
+    app.run_server(debug=True)
